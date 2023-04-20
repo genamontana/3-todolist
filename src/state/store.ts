b0a23e5239d4145c2855097d4863f85e838030ba
genamontana <predator199@yandex.ru>
2023-04-20 17:57:14 +0300

    12 Wednesday 04 completed

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,13 @@
 import {tasksReducer} from './tasks-reducer';
 import {todoListsReducer} from './todolists-reducer';
-import {combineReducers, createStore} from 'redux';
+import {combineReducers, legacy_createStore} from 'redux';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todoLists: todoListsReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = legacy_createStore(rootReducer)
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
